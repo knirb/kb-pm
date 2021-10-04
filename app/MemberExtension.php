7dd7179468a6a8a6ea0ddcc56587cdf35c4c8c4f
Viktor Åsbrink <viktor@kreationsbyran.se>
2021-10-04 22:08:49 +0200

    fix member task error

--- app/MemberExtension.php
+++ app/MemberExtension.php
@@ -1,11 +1,15 @@
 <?php
 
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\ORM\Filterable;
 
 class MemberExtension extends DataExtension
 {
   private static $belongs_many_many = [
     "Projects" => Project::class,
+  ];
+
+  private static $has_many = [
     "Tasks" => Task::class,
   ];
 }
